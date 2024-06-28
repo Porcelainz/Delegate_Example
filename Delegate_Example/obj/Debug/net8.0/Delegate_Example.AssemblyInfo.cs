7b43d4ad88b20e9795b3010610473e83c2c70def
Porcelain_ <ai.red.porcelain@gmail.com>
2024-06-28 16:50:50 +0800

    commit

--- Delegate_Example/obj/Debug/net8.0/Delegate_Example.AssemblyInfo.cs
+++ Delegate_Example/obj/Debug/net8.0/Delegate_Example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delegate_Example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec656cb7ae50e12cc6de06d494ff62086d4f39c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delegate_Example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delegate_Example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
